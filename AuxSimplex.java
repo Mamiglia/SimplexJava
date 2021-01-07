2340d5fae7cf40a86276a24f5b729f4b02a0e6d2
Matteo Migliarini <33762284+Mamiglia@users.noreply.github.com>
2021-01-07 10:53:19 +0100

    updated readability

--- AuxSimplex.java
+++ AuxSimplex.java
@@ -3,20 +3,22 @@ import org.ejml.simple.SimpleMatrix; // must use v0.33 since recent versions don
 import java.util.ArrayList;
 
 class AuxSimplex extends Simplex {
-    // Creates the auxiliar problem, to find out:
-    // 1- whether the problem is non-void
-    // 2- find a valid base
-    // the auxiliar problem has this form:
-    // min (1,1,...,1,1) a
-    //     Ax + Ia = b
-    //     x>=0
-    //     a>=0
-    // a are named "Auxiliar Variables"
-    private ArrayList<Integer> indexofAux;
-    private ArrayList<Integer> indexofRedundant;
+    /**
+     * Creates the auxiliar problem, to find out:
+     * 1- whether the problem is non-void
+     * 2- find a valid base
+     * the auxiliar problem has this form:
+     * min (1,1,...,1,1) a
+     *     Ax + Ia = b
+     *     x>=0
+     *     a>=0
+     * a are named "Auxiliar Variables"
+     */
+    private ArrayList<Integer> indexofAux; // indexes of Auxiliar Variables
+    private ArrayList<Integer> indexofRedundant; // indexes of constraints which are linear dependent from other
 
     public AuxSimplex(SimpleMatrix A, SimpleMatrix b) {
-        // It doesn't check if there are already some identity columns
+        // It doesn't check if there are already some identity columns TODO
         super(
                 cAux(A.numCols(), A.numRows()),
                 A.concatColumns(SimpleMatrix.identity(A.numRows())),
@@ -30,11 +32,11 @@ class AuxSimplex extends Simplex {
         while (columnToBeExited>=0) {
             System.out.println("Column to be exited: " + columnToBeExited);
 
-            int i=0;
+            int i = 0;
             int columnToBeEntered = indexofN.get(i);
             while (indexofAux.contains(columnToBeEntered)) {
-                columnToBeEntered = indexofN.get(i);
-                i++;
+                columnToBeEntered = indexofN.get(i++);
+
             }
             System.out.println("column to enter (K) : " + columnToBeEntered);
 
@@ -68,15 +70,18 @@ class AuxSimplex extends Simplex {
         updateParams();
 
     }
+
     public ArrayList<Integer> getIndexofRedundant() {
         return indexofRedundant;
     }
+
     private int findAuxCol(ArrayList<Integer> indexof) {
         for (Integer i : indexofAux) {
             if (indexof.contains(i)) return i;
         }
         return -1;
     }
+
     static private SimpleMatrix cAux(int n, int m) {
         // creates the auxiliary object function (1,1,1,1,1,...,1)
         SimpleMatrix cAux = new SimpleMatrix(m, 1);
--- Simplex.java
+++ Simplex.java
@@ -6,29 +6,38 @@ import org.ejml.simple.SimpleMatrix; // must use v0.33 since recent versions don
 
 
 public class Simplex {
-    // Solves minimization problems in the standard form:
-    // min c^T x
-    //     Ax=b
-    //     x>=0
+    /**
+     * Solves minimization problems in the standard form:
+     * min c^T x
+     *     Ax=b
+     *     x>=0
+     */
     public final static SimpleMatrix BOUNDLESS_SOLUTION = new SimpleMatrix(new double[][] {{Double.NEGATIVE_INFINITY}});
     public final static SimpleMatrix NONEXISTENT_SOLUTION = new SimpleMatrix(0,0);
-    protected final SimpleMatrix objectiveFunction;
-    protected SimpleMatrix constraintsMatrix;
-    protected SimpleMatrix constraintsVector;
-    protected ArrayList<Integer> indexofB;
-    protected ArrayList<Integer> indexofN;
-    private SimpleMatrix cN;
-    private SimpleMatrix cB;
-    protected SimpleMatrix base;
-    protected SimpleMatrix nonBase;
-    private SimpleMatrix gamma;
+    protected final SimpleMatrix objectiveFunction; // c
+    protected SimpleMatrix constraintsMatrix; // A
+    protected SimpleMatrix constraintsVector; // b
+    protected ArrayList<Integer> indexofB; // indexes of variables in the current base.
+    protected ArrayList<Integer> indexofN; // indexes of variables left out from base.
+    private SimpleMatrix cB; // vector of objective function with only the indexes of B.
+    private SimpleMatrix cN; // vector of objective function with only the indexes not in B.
+    protected SimpleMatrix base; // matrix with columns indexofB.
+    protected SimpleMatrix nonBase; // matrix with columns indexofN.
+    private SimpleMatrix gamma; // vector of reduced costs, it's calculated in the generateGamma() method
     private SimpleMatrix optimalSolution;
 
+    /**
+     * Creates a new Simplex object which can be used to solve a linear problem
+     * @param objectiveFunction the vector representing the coefficients of the objectiveFunction (c), dimensions must be nx1
+     * @param constraintsMatrix the matrix representing the constraints (A), dimensions must be mxn
+     * @param constraintsVector the vector representing the constraints (b), dimensions must be mx1
+     */
     public Simplex(SimpleMatrix objectiveFunction, SimpleMatrix constraintsMatrix, SimpleMatrix constraintsVector) {
         this.objectiveFunction = objectiveFunction;
         this.constraintsMatrix = constraintsMatrix;
         this.constraintsVector = constraintsVector;
     }
+
     public Simplex(double[] objectiveFunction, double[][] constraintsMatrix, double[] constraintsVector) {
         this(
                 convertArrayToSimpleMatrix(objectiveFunction, false),
@@ -49,7 +58,7 @@ public class Simplex {
         }
         indexofB = aux.getIndexBase();
         indexofN = new ArrayList<>();
-        for (int i=0; i<numOfVars(); i++) {
+        for (int i = 0; i<numOfVars(); i++) {
             if(!indexofB.contains(i)) indexofN.add(i);
         }
 
@@ -63,9 +72,9 @@ public class Simplex {
 
     private void removeRedundant(ArrayList<Integer> indexofRedundant) {
         SimpleMatrix AReducted = null, bReducted=null;
-        for (int i=0; i<constraintsMatrix.numRows(); i++) {
+        for (int i = 0; i<constraintsMatrix.numRows(); i++) {
             if (!indexofRedundant.contains(i)) {
-                if (AReducted == null || constraintsVector ==null) {
+                if (AReducted == null || constraintsVector == null) {
                     AReducted = constraintsMatrix.extractVector(true, i);
                     bReducted = constraintsVector.extractVector(true, i);
                 } else {
@@ -81,7 +90,7 @@ public class Simplex {
     public SimpleMatrix solve() {
         if (!checkAmmissibility()) return null;
         initialize();
-        int j=0;
+        int j = 0;
         while (optimalSolution == null) {
             System.out.println("\n\nIteration " + j);
             updateParams();
@@ -99,7 +108,7 @@ public class Simplex {
             } else {
                 System.out.println("No Criterion is verified");
 
-                int h= indexOfMinimum(gamma); //index of N to be entered in B'
+                int h = indexOfMinimum(gamma); //index of N to be entered in B'
 
                 int k; //index of B to be exited from B'
                 SimpleMatrix BinvertedNcolumnh = base.invert().mult(nonBase).extractVector(false, h);
@@ -107,7 +116,7 @@ public class Simplex {
                 ArrayList<Double> roArray = new ArrayList<>();
 
                 //populate roArray
-                for (int i=0; i<BinvertedNcolumnh.numRows(); i++) {
+                for (int i = 0; i<BinvertedNcolumnh.numRows(); i++) {
                     double pihi = BinvertedNcolumnh.get(i,0);
                     if
                         (pihi<=0) roArray.add(Double.POSITIVE_INFINITY);
@@ -133,14 +142,21 @@ public class Simplex {
     public SimpleMatrix getOptimalSolution() {
         return optimalSolution.copy();
     }
+
     public double getOptimalValue() {
-        if (optimalSolution.equals(BOUNDLESS_SOLUTION)) return Double.NEGATIVE_INFINITY;
-        else if (optimalSolution.equals(NONEXISTENT_SOLUTION)) return Double.POSITIVE_INFINITY;
-        else return objectiveFunction.transpose().mult(optimalSolution).get(0,0);
+        if (optimalSolution.equals(BOUNDLESS_SOLUTION)) {
+            return Double.NEGATIVE_INFINITY;
+        } else if (optimalSolution.equals(NONEXISTENT_SOLUTION)) {
+            return Double.POSITIVE_INFINITY;
+        } else {
+            return objectiveFunction.transpose().mult(optimalSolution).get(0,0);
+        }
     }
+
     public int numOfConstraints() {
         return constraintsMatrix.numRows();
     }
+
     public int numOfVars() {
         return constraintsMatrix.numCols();
     }
@@ -151,32 +167,35 @@ public class Simplex {
         SimpleMatrix xUnordered = xB.concatRows(xN);
         SimpleMatrix xStar = new SimpleMatrix(xUnordered.numRows(), 1);
 
-        for (int i=0; i<indexofB.size(); i++) {
+        for (int i = 0; i<indexofB.size(); i++) {
             xStar.set(indexofB.get(i),0,xUnordered.get(i,0));
         }
-        for (int i=0; i<indexofN.size(); i++) {
+        for (int i = 0; i<indexofN.size(); i++) {
             xStar.set(indexofN.get(i),0,xUnordered.get(i+indexofB.size(),0));
         }
         return xStar;
     }
+
     protected void updateParams() {
         generateBase();
         generateGamma();
     }
+
     private void generateGamma() {
         gamma = (cN.transpose().minus(cB.transpose().mult(base.invert().mult(nonBase)))).transpose();
     }
+
     protected void generateBase() {
         base = null;
         nonBase = null;
         cB = new SimpleMatrix(numOfConstraints(), 1);
         cN = new SimpleMatrix(numOfVars()-numOfConstraints(),1);
-        for (int i=0; i<indexofB.size();i++) {
+        for (int i = 0; i<indexofB.size();i++) {
             if (base == null) base = constraintsMatrix.extractVector(false,indexofB.get(i));
             else base = base.concatColumns(constraintsMatrix.extractVector(false,indexofB.get(i)));
             cB.set(i,0,objectiveFunction.get(indexofB.get(i),0));
         }
-        for (int j=0; j<indexofN.size();j++) {
+        for (int j = 0; j<indexofN.size();j++) {
             if (nonBase == null) nonBase = constraintsMatrix.extractVector(false,indexofN.get(j));
             else nonBase = nonBase.concatColumns(constraintsMatrix.extractVector(false,indexofN.get(j)));
             cN.set(j,0,objectiveFunction.get(indexofN.get(j),0));
@@ -195,14 +214,15 @@ public class Simplex {
         );
     }
     private boolean optimalityCriterion() {
-        for (int i=0; i<gamma.numRows(); i++) {
+        for (int i = 0; i<gamma.numRows(); i++) {
             if (gamma.get(i,0) < 0) return false;
         }
         return true;
     }
+
     private boolean unlimitednessCriterion() {
         SimpleMatrix BinvertedN = base.invert().mult(nonBase);
-        for (int i=0; i<gamma.numRows(); i++) {
+        for (int i = 0; i<gamma.numRows(); i++) {
             if (
                     gamma.get(i,0) < 0
                     && isMatrixNonPositive(BinvertedN.extractVector(false,i))
@@ -212,6 +232,7 @@ public class Simplex {
         }
         return false;
     }
+
     private boolean checkAmmissibility() {
         if (
                 constraintsVector.numRows() == numOfConstraints() &&
@@ -236,6 +257,7 @@ public class Simplex {
 
         return c.indexOf(findMinimum(c));
     }
+
     public static double findMinimum(ArrayList<Double> v) {
         double min = Double.POSITIVE_INFINITY;
         for (Double aDouble : v) {
@@ -243,12 +265,14 @@ public class Simplex {
         }
         return min;
     }
+
     public static boolean isMatrixNonPositive(SimpleMatrix m) {
-        for (int ii=0; ii<m.numRows()*m.numCols(); ii++) {
-            if (m.get(ii)>0) return false;
+        for (int i = 0; i<m.numRows()*m.numCols(); i++) {
+            if (m.get(i)>0) return false;
         }
         return true;
     }
+
     public static SimpleMatrix convertArrayToSimpleMatrix(double[] a, boolean horizontal) {
         int n = (horizontal?1:a.length);
         int m = (horizontal?a.length:1);
